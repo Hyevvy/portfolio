b41d3ea2e45d617daba53672c12dd6650c1825c2
Hyevvy <hyeg0@naver.com>
2021-12-23 03:58:04 +0900

    JS selected Button

--- main.js
+++ main.js
@@ -63,6 +63,13 @@ workCategory.addEventListener("click", (e) => {
   if (filter == null) {
     return;
   }
+
+  //Remove selection from the previous item and select the new one;
+  const active = document.querySelector(".category__btn.selected");
+  active.classList.remove("selected");
+  const target = e.target.nodeName == "BUTTON" ? e.target : e.target.parentNode;
+  target.classList.add("selected");
+
   projectContainer.classList.add("anim-out");
   setTimeout(() => {
     projects.forEach((project) => {
@@ -74,8 +81,6 @@ workCategory.addEventListener("click", (e) => {
     });
     projectContainer.classList.remove("anim-out");
   }, 300);
-
-  console.log(filter);
 });
 
 function scrollIntoView(selector) {
