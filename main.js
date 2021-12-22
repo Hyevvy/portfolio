938597c2cf7795e8e9aae5abd875e613f4c05b38
Hyevvy <hyeg0@naver.com>
2021-12-22 23:19:43 +0900

    JS navbar menu click

--- main.js
+++ main.js
@@ -1,6 +1,12 @@
 "use strict";
 const navbar = document.querySelector("#navbar");
 const navbarHeight = navbar.getBoundingClientRect().height;
+const contact = document.querySelector("#contact");
+const home = document.querySelector("#home");
+const about = document.querySelector("#about");
+const work = document.querySelector("#work");
+const skills = document.querySelector("#skills");
+const testimonails = document.querySelector("#testimonials");
 
 // Make navbar transparent when it is on the top
 document.addEventListener("scroll", () => {
@@ -9,4 +15,20 @@ document.addEventListener("scroll", () => {
   } else {
     navbar.classList.remove("navbar--dark");
   }
+});
+
+//Handle scrolling when tapping on the navbar menu
+const homeBtn = document.querySelector(".home__btn");
+homeBtn.addEventListener("click", (e) => {
+  window.scrollTo({ top: contact.offsetTop, behavior: "smooth" });
+});
+const navbarMenu = document.querySelector(".navbar__menu");
+navbarMenu.addEventListener("click", (e) => {
+  const target = e.target;
+  const link = target.dataset.link;
+  if (link == null) {
+    return;
+  }
+  const item = document.querySelector(link);
+  window.scrollTo({ top: item.offsetTop, behavior: "smooth" });
 });
